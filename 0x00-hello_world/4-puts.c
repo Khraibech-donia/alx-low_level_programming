25b3da66ac02f18dbb451d47c10da85520d40660
Khraibech-donia <donia.khraibech207@gmail.com>
2023-06-15 09:10:51 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
-
 /**
-*
-* main - Entry point 
-*
-* description: program that prints a line with puts fonctio
-*
-* Return: 0 (Success)
+ *main - program that prints a line with puts fonction
+*Return: 0 (Success)
 */
 int main(void)
-{
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
-
-}               
+ {
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}  
