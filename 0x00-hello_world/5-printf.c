2a1378e8b4ff1e06dd747f01a6c86681e12a976f
Khraibech-donia <donia.khraibech207@gmail.com>
2023-06-15 11:45:15 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
-*
-*description: A C program that print with printf function.
-*
-*Return: Always 0 (Success)
-*/
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
- {
-      printf("Programming is like building a multilingual puzzle,\n");
-      return (0);
-}                      
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
