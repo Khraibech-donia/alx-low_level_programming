a6d380d43fc5b7ab6836b907f5479942973d8b31
Khraibech-donia <donia.khraibech207@gmail.com>
2023-06-15 09:04:03 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
 * main - program that prints a line with puts fonction
 * Return: 0 (Success)
 */
-int main(void);
+int main(void)
 {
    puts("\"Programming is like building a multilingual puzzle");
    return (0);
+
 }               
