2f1faf27950c02eea835ab29f258424c6deda1ab
Khraibech-donia <donia.khraibech207@gmail.com>
2023-06-15 09:22:17 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - program that prints a line with puts fonction
+*Rrturn: 0 (Success)
+*/
+int main(void)
+ {
+printf("Programming is like building a multilingual puzzle,\n");
+return (0);
+}                   
