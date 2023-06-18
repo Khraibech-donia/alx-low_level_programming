add8f5ad1842d15e4b4b409375ed1e47bb6b2872
Khraibech-donia <donia.khraibech207@gmail.com>
2023-06-18 11:43:18 -0700

    write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,10 @@
 /**
  * main - Entry point
  *
- * description: print with write function
- *
- * return 1 (success)
+ * Return: always 1 (Success)
  */
 int main(void)
 {
-	write(3, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
