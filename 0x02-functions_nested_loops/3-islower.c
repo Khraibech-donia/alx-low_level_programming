05b8946d55bee73a8025e3436be58b07fba0405b
Khraibech-donia <donia.khraibech207@gmail.com>
2023-06-20 12:32:02 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main : function that checks for lowercase character
+ * main: function that checks for lowercase character
  *
  * Return: 1 if char is lowercase, otherwise 0.
  */
