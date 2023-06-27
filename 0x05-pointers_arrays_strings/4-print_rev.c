3d14eab31de5ffaa7da8c004f0de98b5877a517c
Khraibech-donia <donia.khraibech207@gmail.com>
2023-06-26 17:00:04 -0700

    k

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,15 +1,27 @@
 #include "main.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
  */
-int main(void)
+void print_rev(char *s)
 {
-	char *str;
+	int longi = 0
 
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	print_rev(str);
-	return (0);
+	int o
+
+	while (*s != '\0')
+	{
+	longi++;
+	s++;
+	}
+	s--;
+	for (o = longi; o > 0; o--)
+	{
+	_putchar(*s);
+	s--;
+	}
+
+	_putchar('\n');
 }
+
