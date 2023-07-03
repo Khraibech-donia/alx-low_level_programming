b3cc226d32446076cb8ebe906344491ec15c6d3f
Khraibech-donia <donia.khraibech207@gmail.com>
2023-07-03 06:31:03 -0700

    ok

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ *_memcpy - a function that copies memory area
+ *@dest: memory where is stored
+ *@src: memory where is copied
+ *@n: number of bytes
+ *
+ *Return: copied memory with n bytes changed
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int r = 0;
+	int i = n;
+
+	for (; r < i; r++)
+	{
+	dest[r] = src[r];
+	n--;
+	}
+	return (dest);
+}
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char buffer[98] = {0};
+	char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+
+	simple_print_buffer(buffer, 98);
+	_memcpy(buffer + 50, buffer2, 10);
+	printf("-------------------------------------------------\n");
+	simple_print_buffer(buffer, 98);
+	return (0);
+}
