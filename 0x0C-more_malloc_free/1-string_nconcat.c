77c5bd84211eae035307fabdd5decac29294c424
Khraibech-donia <donia.khraibech207@gmail.com>
2023-07-13 10:15:56 -0700

    ok

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,6 +15,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 
 	char *s;
+
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 	while (s1 && s1[len1])
@@ -28,7 +29,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n < len2)
 
 	s = malloc(sizeof(char) * (len1 + n + 1));
-
 	else
 
 	s = malloc(sizeof(char) * (len1 + len2 + 1));
@@ -45,7 +45,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	i++;
 
-	}
+}
 
 	while (n < len2 && i < (len1 + n))
 
@@ -58,6 +58,4 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	s[i] = '\0';
 
 	return (s);
-
 }
-
